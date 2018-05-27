0df5371f7f25088796c5e3b737e8e113cf1715f7
silentwhaleluo <jingyuluoch@outlook.com>
2018-05-26 23:59:50 -0400

    Add files via upload

---
+++ Step3_PCA.py
@@ -0,0 +1,56 @@
+
+
+import pandas as pd
+import numpy as np
+from sklearn.decomposition import PCA 
+path='/SOT/'
+train_df=pd.read_csv(path+"train_1000.csv")
+
+#test the data accuracy
+stat=train_df['Sentiment'].value_counts()
+print(stat)
+train=np.array(train_df)
+X=train[:,2:]
+print(X.shape)
+#See the variance of all dimension to choice the number of dimension.
+
+  
+pca = PCA(copy=True, n_components=None, whiten=False)
+pca.fit(X)  
+var=pca.explained_variance_ratio_
+PCA_ANALYSIS=[]
+cul=0
+for i in range(len(var)):
+    cul=cul+var[i]
+    PCA_ANALYSIS=[[i,var[i],cul]]
+    print(i,cul)
+
+PCA_ANALYSIS=DataFrame(PCA_ANALYSIS,columns=['component','var','cumulative_var'])
+PCA_ANALYSIS.to_csv(path+'PCA_RESULTS.csv',index_label ='ID',header=True)
+print('PCA results saved to', path)
+
+train[:,1].sum()
+print(X.shape)
+pca = PCA(copy=True, n_components=369, whiten=False) 
+pca.fit(X)  
+
+#PCA(copy=True, n_components=None, whiten=False)  
+var=pca.explained_variance_ratio_
+print(len(var))
+
+print(var.shape)
+X_DR=pca.transform(X)
+print(X_DR.shape)
+
+col_name=[]
+for i in range(1,len(X_DR[0])+1):
+    col_name.append('X'+str(i))
+len(col_name)
+from pandas import DataFrame
+df_DR=DataFrame(X_DR,columns=col_name)
+df_DR['sentiment']=train[:,1]
+df_DR = df_DR[['sentiment']+col_name]
+
+df_DR.to_csv(path+'train_1000_DR.csv',index_label ='ID',header=True)
+print('DR train data saved to', path)
+
